3c439a4f4a0325507747f881cdff7491e9c83457
Tyler Johnson <tylerl.johnson@gmail.com>
2022-12-13 10:47:23 -0500

    2022 Day 13: Distress Signal

--- lib/advent_of_code/2022/day_13.ex
+++ lib/advent_of_code/2022/day_13.ex
@@ -1,40 +1,68 @@
 defmodule AdventOfCode.Y2022.Day13 do
-  defdelegate parse(input), to: __MODULE__.Input
+  alias __MODULE__.Input
+
+  @keys [[[2]], [[6]]]
 
   def part1(input \\ nil) do
     input
-    |> parse()
-    |> Enum.map(& &1)
+    |> Input.parse()
+    |> Stream.with_index(1)
+    |> Stream.map(fn {[a, b], index} ->
+      if correct?(a, b), do: index, else: 0
+    end)
+    |> Enum.sum()
   end
 
   def part2(input \\ nil) do
-    input
-    |> parse()
-    |> Enum.map(& &1)
+    (@keys ++ Input.parse_lines(input))
+    |> Enum.sort(fn a, b -> correct?(a, b) end)
+    |> Stream.with_index(1)
+    |> Stream.map(fn {v, index} ->
+      if v in @keys, do: index, else: 1
+    end)
+    |> Enum.product()
+  end
+
+  def correct?([], []), do: nil
+  def correct?([], _), do: true
+  def correct?(_, []), do: false
+  def correct?([a | t1], [a | t2]), do: correct?(t1, t2)
+  def correct?([a | _], [b | _]) when is_integer(a) and is_integer(b) and a < b, do: true
+  def correct?([a | _], [b | _]) when is_integer(a) and is_integer(b) and a > b, do: false
+
+  def correct?([a | t1], [b | t2]) when is_list(a) and not is_list(b),
+    do: correct?([a | t1], [[b] | t2])
+
+  def correct?([a | t1], [b | t2]) when not is_list(a) and is_list(b),
+    do: correct?([[a] | t1], [b | t2])
+
+  def correct?([a | t1], [b | t2]) when is_list(a) and is_list(b) do
+    case correct?(a, b) do
+      nil -> correct?(t1, t2)
+      result -> result
+    end
   end
 
   defmodule Input do
     @day 13
     @year 2022
 
-    def parse(nil) do
-      AdventOfCode.Input.get!(@day, @year) |> parse()
-    end
+    def parse(nil), do: remote_input() |> parse()
 
-    def parse(input) when is_binary(input) do
+    def parse(input) do
       input
+      |> String.split("\n\n", trim: true)
+      |> Stream.map(&parse_lines/1)
+    end
+
+    def parse_lines(nil), do: remote_input() |> parse_lines()
+
+    def parse_lines(lines) do
+      lines
       |> String.split("\n", trim: true)
-      |> parse()
+      |> Enum.map(&(Code.eval_string(&1) |> elem(0)))
     end
 
-    def parse(input) when is_list(input) do
-      Stream.map(input, &parse_line/1)
-    end
-
-    def parse_line(line) do
-      line
-      # |> String.split(["-", ","], trim: true)
-      # |> Enum.map(&String.to_integer/1)
-    end
+    def remote_input(), do: AdventOfCode.Input.get!(@day, @year)
   end
 end
--- test/advent_of_code/2022/day_13_test.exs
+++ test/advent_of_code/2022/day_13_test.exs
@@ -1,23 +1,24 @@
 defmodule AdventOfCode.Y2022.Day13Test do
   use ExUnit.Case
 
-  import AdventOfCode.Y2022.Day13
+  alias AdventOfCode.Y2022.Day13
 
   setup_all do
     [
-      input: ""
+      input:
+        "[1,1,3,1,1]\n[1,1,5,1,1]\n\n[[1],[2,3,4]]\n[[1],4]\n\n[9]\n[[8,7,6]]\n\n[[4,4],4,4]\n[[4,4],4,4,4]\n\n[7,7,7,7]\n[7,7,7]\n\n[]\n[3]\n\n[[[]]]\n[[]]\n\n[1,[2,[3,[4,[5,6,7]]]],8,9]\n[1,[2,[3,[4,[5,6,0]]]],8,9]"
     ]
   end
 
   describe "part1" do
     test "returns expected value", %{input: input} do
-      assert part1(input) == []
+      assert Day13.part1(input) == 13
     end
   end
 
   describe "part2" do
     test "returns expected value", %{input: input} do
-      assert part2(input) == []
+      assert Day13.part2(input) == 140
     end
   end
 end
