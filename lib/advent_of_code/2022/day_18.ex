1d3051fff0d5e1837ed67890ee9987c970063fbd
Tyler Johnson <tylerl.johnson@gmail.com>
2022-12-18 16:55:06 -0500

    2022 Day 18: Boiling Boulders - Part 1

--- lib/advent_of_code/2022/day_18.ex
+++ lib/advent_of_code/2022/day_18.ex
@@ -2,15 +2,33 @@ defmodule AdventOfCode.Y2022.Day18 do
   defdelegate parse(input), to: __MODULE__.Input
 
   def part1(input \\ nil) do
-    input
-    |> parse()
-    |> Enum.map(& &1)
+    grid = input |> parse() |> MapSet.new()
+
+    grid
+    |> Enum.map(fn xyz ->
+      6 - neighbor_count(grid, xyz)
+    end)
+    |> Enum.sum()
   end
 
   def part2(input \\ nil) do
     input
     |> parse()
     |> Enum.map(& &1)
+  end
+
+  def neighbor_count(set, {x, y, z}) do
+    [
+      {1, 0, 0},
+      {-1, 0, 0},
+      {0, 1, 0},
+      {0, -1, 0},
+      {0, 0, 1},
+      {0, 0, -1}
+    ]
+    |> Enum.count(fn {a, b, c} ->
+      MapSet.member?(set, {a + x, b + y, c + z})
+    end)
   end
 
   defmodule Input do
@@ -33,8 +51,9 @@ defmodule AdventOfCode.Y2022.Day18 do
 
     def parse_line(line) do
       line
-      # |> String.split(["-", ","], trim: true)
-      # |> Enum.map(&String.to_integer/1)
+      |> String.split(",", trim: true)
+      |> Enum.map(&String.to_integer/1)
+      |> List.to_tuple()
     end
   end
 end
