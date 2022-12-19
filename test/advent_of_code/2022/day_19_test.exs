a539f1a62904b7b247fcac327c2c3111a2354857
Tyler Johnson <tylerl.johnson@gmail.com>
2022-12-18 21:09:10 -0500

    2022 Day 19 template

---
+++ test/advent_of_code/2022/day_19_test.exs
@@ -0,0 +1,23 @@
+defmodule AdventOfCode.Y2022.Day19Test do
+  use ExUnit.Case
+
+  alias AdventOfCode.Y2022.Day19
+
+  setup_all do
+    [
+      input: ""
+    ]
+  end
+
+  describe "part1" do
+    test "returns expected value", %{input: input} do
+      assert Day19.part1(input) == []
+    end
+  end
+
+  describe "part2" do
+    test "returns expected value", %{input: input} do
+      assert Day19.part2(input) == []
+    end
+  end
+end
