9a9289677edb857568d9e86dfbab6683a5c7c261
Tyler Johnson <tylerl.johnson@gmail.com>
2022-12-21 09:26:43 -0500

    2022 Day 21: Monkey Math - code and test

--- lib/advent_of_code/2022/day_21.ex
+++ lib/advent_of_code/2022/day_21.ex
@@ -4,13 +4,67 @@ defmodule AdventOfCode.Y2022.Day21 do
   def part1(input \\ nil) do
     input
     |> parse()
-    |> Enum.map(& &1)
+    |> solve("root")
+    |> trunc()
   end
 
   def part2(input \\ nil) do
     input
     |> parse()
-    |> Enum.map(& &1)
+    |> Map.update!("root", fn {_, a, b} ->
+      {:=, a, b}
+    end)
+    |> search(1, 0)
+  end
+
+  def solve(map, key) when is_binary(key), do: solve(map, map[key])
+  def solve(_, num) when is_integer(num), do: num
+
+  def solve(map, {:=, a, b}) do
+    a = solve(map, a) |> trunc()
+    b = solve(map, b) |> trunc()
+
+    {a - b, a, b}
+  end
+
+  def solve(map, {op, a, b}),
+    do: apply(Kernel, op, [solve(map, a), solve(map, b)])
+
+  def search(map, root, exponent) do
+    value = root * 10 ** exponent
+
+    map
+    |> Map.put("humn", value)
+    |> solve("root")
+    # |> then(fn {diff, _, _} = res ->
+    #   IO.puts("DIFF: #{diff}, VALUE: #{value}")
+    #   res
+    # end)
+    |> handle_result(map, root, exponent)
+  end
+
+  def handle_result({0, _, _}, _map, root, exp), do: root * 10 ** exp
+
+  def handle_result({diff, _, _}, map, 1, 0) when diff < 0 do
+    map
+    |> Map.update!("root", fn {:=, a, b} -> {:=, b, a} end)
+    |> search(1, 0)
+  end
+
+  def handle_result({diff, _, _}, map, 1, exp) when diff > 0 do
+    search(map, 1, exp + 1)
+  end
+
+  def handle_result(_, map, 1, exp) do
+    search(map, 2, exp - 1)
+  end
+
+  def handle_result({diff, _, _}, map, root, exp) when diff > 0 do
+    search(map, root + 1, exp)
+  end
+
+  def handle_result(_, map, root, exp) do
+    search(map, (root - 1) * 10 + 1, exp - 1)
   end
 
   defmodule Input do
@@ -28,13 +82,18 @@ defmodule AdventOfCode.Y2022.Day21 do
     end
 
     def parse(input) when is_list(input) do
-      Stream.map(input, &parse_line/1)
+      input
+      |> Stream.map(&parse_line/1)
+      |> Enum.into(%{})
     end
 
     def parse_line(line) do
       line
-      # |> String.split(["-", ","], trim: true)
-      # |> Enum.map(&String.to_integer/1)
+      |> String.split([": ", " "], trim: true)
+      |> then(fn
+        [name, number] -> {name, String.to_integer(number)}
+        [name, a, op, b] -> {name, {String.to_atom(op), a, b}}
+      end)
     end
   end
 end
--- test/advent_of_code/2022/day_21_test.exs
+++ test/advent_of_code/2022/day_21_test.exs
@@ -5,19 +5,20 @@ defmodule AdventOfCode.Y2022.Day21Test do
 
   setup_all do
     [
-      input: ""
+      input:
+        "root: pppw + sjmn\ndbpl: 5\ncczh: sllz + lgvd\nzczc: 2\nptdq: humn - dvpt\ndvpt: 3\nlfqf: 4\nhumn: 5\nljgn: 2\nsjmn: drzm * dbpl\nsllz: 4\npppw: cczh / lfqf\nlgvd: ljgn * ptdq\ndrzm: hmdt - zczc\nhmdt: 32"
     ]
   end
 
   describe "part1" do
     test "returns expected value", %{input: input} do
-      assert Day21.part1(input) == []
+      assert Day21.part1(input) == 152
     end
   end
 
   describe "part2" do
     test "returns expected value", %{input: input} do
-      assert Day21.part2(input) == []
+      assert Day21.part2(input) == 301
     end
   end
 end
