e7d798554995a8a5dd345d7b05eea0afb0406310
Tyler Johnson <tylerl.johnson@gmail.com>
2023-01-07 15:36:28 -0500

    2016 Day 1: No Time for a Taxicab - part 1

---
+++ lib/advent_of_code/2016/template_day_01.ex
@@ -0,0 +1,40 @@
+defmodule AdventOfCode.Y2016.TemplateDay01 do
+  defdelegate parse(input), to: __MODULE__.Input
+
+  def part1(input \\ nil) do
+    input
+    |> parse()
+    |> Enum.map(& &1)
+  end
+
+  def part2(input \\ nil) do
+    input
+    |> parse()
+    |> Enum.map(& &1)
+  end
+
+  defmodule Input do
+    @day 1
+    @year 2016
+
+    def parse(nil) do
+      AdventOfCode.Input.get!(@day, @year) |> parse()
+    end
+
+    def parse(input) when is_binary(input) do
+      input
+      |> String.split("\n", trim: true)
+      |> parse()
+    end
+
+    def parse(input) when is_list(input) do
+      Stream.map(input, &parse_line/1)
+    end
+
+    def parse_line(line) do
+      line
+      # |> String.split(["-", ","], trim: true)
+      # |> Enum.map(&String.to_integer/1)
+    end
+  end
+end
